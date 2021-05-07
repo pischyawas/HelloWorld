4a2ad6d4c70ecba3ef1c70302116808fed7517a8
pischyawas <83536733+pischyawas@users.noreply.github.com>
2021-05-08 03:05:13 +0700

    Change Name File + Add new Project

---
+++ DataAccess/DataAccess.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQliteapp
+{
+    class DataAccess
+    {
+        public static void InitializeDatabase()
+        {
+
+        }
+
+            public static void AddData(string inputText)
+         {
+
+            using (SqliteConnection db = 
+                new SqliteConnection("Filename=sqliteSample.db"))
+            {
+                db.Open();
+
+                SqliteCommand insertCommand = new SqliteCommand();
+                insertCommand.Connection = db;
+
+
+                insertCommand.CommandText = "INSERT INTO MYTable VALUES (NULL, @Entry);";
+                insertCommand.Parameters.AddWithValue("@Entry", inputText);
+                
+                insertCommand.ExecuteReader();
+                db.Close();
+            }
+
+          }
+        public static List<String> GetData()
+        {
+            List<String> entries = new List<string>();
+            using (SqliteConnection db =
+               new SqliteConnection($"Filename=sqliteSample.db"))
+            {
+                db.Open();
+                SqliteCommand selectCommand = new SqliteCommand
+                    ("SELECT Text_Entry from MyTable", db);
+                SqliteDataReader query = selectCommand.ExecuteReader();
+                
+                while (query.Read())
+                {
+                    entries.Add(query.GetString(0));
+                }
+                db.Close();
+            }
+            return entries;
+        }
+    }
+}
---
+++ DataAccess/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQliteapp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataAccess.InitializeDatabase();
+            DataAccess.AddData("Pish Sirichainakorn");
+            DataAccess.GetData();
+        }
+
+        private void showall_Click(object sender, RoutedEventArgs e)
+        {
+            string DataUser = " ";
+            foreach (string data in DataAccess.GetData())
+            {
+                DataUser = DataUser + data + "\n";
+            }
+            MessageBox.Show(DataUser);
+        }
+    }
+}
